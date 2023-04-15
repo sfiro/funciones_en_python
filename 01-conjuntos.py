14e29b265920b44bfb3328af8b32bebc6a3f2e7f
Debbie Arredondo <chevi6@gmail.com>
2023-04-15 12:00:41 -0500

    se agrega el conjunto de operaciones permitidas para los conjuntos en python

--- 01-conjuntos.py
+++ 01-conjuntos.py
@@ -25,3 +25,79 @@ print (set_numbers) # {1, 2, 3, 4}
 # si quiero convertir este set único a una lista, lo puedo hacer:
 unique_numbers = list(set_numbers)
 print (unique_numbers)
+
+
+###----------------Modificación de conjuntos ------------------
+
+# evaluar el tamaño del conjunto
+size = len(set_countries)
+print(size)
+
+# evaluar si un elemento se encuentra dentro de un conjunto
+print('col' in set_countries)
+
+# agregar un elemento al conjunto
+set_countries.add("pe")
+print("adiciona el dato 'pe' al conjunto ")
+print(set_countries)
+set_countries.add("bol")
+print("adiciona el dato 'bol' al conjunto ")
+print(set_countries)
+
+#update
+set_countries.update({'ar','ecua','pe'})
+print("agrega un conjunto de datos a un conjunto previo ")
+print(set_countries)
+
+#remover
+set_countries.remove('col')
+print("elimina el elemento 'col' si no existe marca error ")
+print(set_countries)
+
+#remover sin error de falta de elemento
+set_countries.discard('arg')
+print("elimina el elemento 'arg' si no existe no marca error ")
+print(set_countries)
+
+#eliminar todos los elementos 
+set_countries.clear()
+print("elimina todos los elementos del conjunto")
+print(set_countries)
+
+
+## ------------- operaciones con conjuntos -------------------
+
+
+set_a = {'col','mex','bol'}
+print("conjunto A:")
+print(set_a)
+set_b = {'pe','bol'}
+print("conjunto B:")
+print(set_b)
+
+# union de los conjuntos
+
+set_c = set_a.union(set_b)
+print("union de conjuntos A y B")
+print(set_c)
+print(set_a | set_b)
+
+# interseccion
+
+set_c = set_a.intersection(set_b)
+print("intersección de conjuntos A y B")
+print(set_c)
+print(set_a & set_b)
+
+#diferencia entre conjuntos
+
+set_c = set_a.difference(set_b)
+print("diferencia entre conjuntos A y B")
+print(set_c)
+print(set_a-set_b)
+
+#diferencia simetrica
+set_c = set_a.symmetric_difference(set_b)
+print("la diferencia simetrica entre los conjuntos A y B (tomar los conjuntos y eliminar la interseccion")
+print(set_c)
+print(set_a ^ set_b)
