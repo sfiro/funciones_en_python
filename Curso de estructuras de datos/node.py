ae6012858b5283600feba5b5ea6351abbdab00f8
Debbie Arredondo <chevi6@gmail.com>
2023-04-25 23:48:26 -0500

    Funciones para crear nodos como estructura de datos

---
+++ Curso de estructuras de datos/node.py
@@ -0,0 +1,29 @@
+
+class Node():
+    """ Funcion para definir un nodo (estructura de datos) donde se reciben dos parametros, el valor y la referencia al siguiente nodo"""
+    def __init__(self,data,next=None):
+        self.data = data
+        self.next = next
+
+if __name__ == "__main__":
+
+    "Creación de nodos interconectados"
+    nodo1 = None
+    nodo2 = Node("A",None)
+    nodo3 = Node("B",nodo2)
+    print(nodo2.data)
+    print(nodo2.next)
+    print(nodo3.data)
+    print(nodo3.next.data)
+    nodo1 = Node("C",nodo3)
+    print(nodo1.data)
+    print(nodo1.next.data)
+   
+    head=None
+    for i in range(1,5):
+        print(i)
+        head = Node(i,head)
+    
+    while head != None:
+        print(head.data)
+        head = head.next
